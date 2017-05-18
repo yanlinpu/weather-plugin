f3c4c67cc2f9b8469913ebdf1b52e3f415fc4d54
yanlp <yanlp@csdn.net>
2017-05-18 10:43:57 +0800

    hotfix: config from file to arguments.
    Signed-off-by: yanlp <yanlp@csdn.net>

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "db": {
-        "redis": {
-            "host": "10.8.1.6",
-            "port": "6379",
-            "db": 0
-        }
-    },
-};
--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 var that = {};
 that.weather = require("./lib/weather");
-that.weather.init();
-
+that.init = function(options){
+    that.weather.init(options);
+}
 that.getCurrentWeather=that.weather.getCurrentWeather;
 module.exports = that;
